f2f30dab1b3de19a45967fe01b7147752216a018
anvitaa22 <asekhsar@ucsd.edu>
2021-02-13 18:37:09 -0800

    completed

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,79 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+
+    this.elements = {};
+    this.attachShadow({ mode: "open" }); 
+    const wrapper = document.createElement("li");
+    wrapper.setAttribute("class", "product");
+    this.elements.wrapper = wrapper;
+
+    this.elements.img = wrapper.appendChild(document.createElement("img"));
+
+    this.elements.title = document.createElement("p");
+    this.elements.title.setAttribute("class", "title");
+    wrapper.appendChild(this.elements.title);
+
+    this.elements.price = wrapper.appendChild(document.createElement("p"));
+    this.elements.price.setAttribute("class", "price");
+
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", "./styles/styles.css");
+    this.elements.linkElem = linkElem;
+    this.shadowRoot.append(linkElem, wrapper);
+  }
+
+  connectedCallback() {
+    this.elements.img.src = this.getAttribute("image-src");
+    this.elements.img.alt = this.getAttribute("title");
+
+    this.elements.price.innerText = this.getAttribute("price");
+    this.elements.title.innerText = this.getAttribute("title");
+
+    this.elements.cartBtn = this.elements.wrapper.appendChild(
+      document.createElement("button")
+    );
+    this.elements.cartBtn.innerText = "Add to Cart";
+
+    const IN_CART = "1";
+    const NOT_IN_CART = "0";
+    const productId = this.getAttribute("product-id");
+
+    let addToCart = () => {
+      let countElement = document.getElementById("cart-count");
+      countElement.innerText = parseInt(countElement.innerText) + 1;
+      this.elements.cartBtn.innerText = "Remove from Cart";
+      this.elements.cartBtn.onclick = () => {
+        removeFromCart();
+        alert("Removed from cart!");
+      };
+      localStorage.setItem("store.items." + productId, IN_CART);
+    };
+
+    let removeFromCart = () => {
+      let countElement = document.getElementById("cart-count");
+      countElement.innerText = parseInt(countElement.innerText) - 1;
+      this.elements.cartBtn.innerText = "Add to Cart";
+      this.elements.cartBtn.onclick = () => {
+        addToCart();
+        alert("Added to cart!");
+      };
+      localStorage.setItem("store.items." + productId, NOT_IN_CART);
+      
+    };
+    
+    this.elements.cartBtn.onclick = () => {
+      addToCart();
+      alert("Added to cart!");
+    };
+
+    if (localStorage.getItem("store.items." + productId) === IN_CART) {
+      addToCart();
+    }
+  }
 }
 
-customElements.define('product-item', ProductItem);
+customElements.define("product-item", ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,39 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  let objs = { items:[],};
+
+  let store = localStorage.getItem("objs.items");
+  if(store){
+    objs.items = JSON.parse(store);
+    addShopItems(objs.items);
+  }
+  else{
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then((data) => {
+          let serial = JSON.stringify(data);
+          localStorage.setItem("objs.items", serial);
+          objs.items = data;
+          addShopItems(objs.items);
+      });
+  }
 });
+
+  function addShopItems(items) {
+
+    let elements = []
+    items.forEach((item) => {
+      let e = document.createElement("product-item");
+      e.setAttribute("image-src", item.image);
+      e.setAttribute("title", item.title);
+      e.setAttribute("price", "$" + item.price);
+      e.setAttribute("product-id", "$" + item.price);
+      elements.push(e);
+    });
+  
+    elements.forEach((e) => {
+      document.getElementById("product-list").appendChild(e)
+    });
+
+  }
